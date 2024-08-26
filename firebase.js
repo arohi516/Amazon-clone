cbcbead7467f365419406953f91810c9c1223a80
arohi516 <98512971+arohi516@users.noreply.github.com>
2024-08-26 13:17:41 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,11 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAacwKOFoLRUEJ0crtHGjtQNBldUAF_uoM",
+    authDomain: "clone-a6865.firebaseapp.com",
+    databaseURL: "https://clone-a6865.firebaseio.com",
+    projectId: "clone-a6865",
+    storageBucket: "clone-a6865.appspot.com",
+    messagingSenderId: "987512157755",
+    appId: "1:987512157755:web:89a3bcd713f30861200e1e",
+    measurementId: "G-RF6CBW0L0Z"
+  };
